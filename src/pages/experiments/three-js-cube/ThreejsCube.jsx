c083f7df54911370ed9e0e56bd20c6410823dfbe
Gustavo Souza <gustavoelia7@gmail.com>
2025-09-26 20:30:54 -0300

    three.js cube improved code

--- src/pages/experiments/three-js-cube/ThreejsCube.jsx
+++ src/pages/experiments/three-js-cube/ThreejsCube.jsx
@@ -12,28 +12,40 @@ export default function ThreejsCube() {
       1000
     );
 
-    const renderer = new THREE.WebGLRenderer();
-    renderer.setSize(window.innerWidth, window.innerHeight);
-    renderer.setAnimationLoop(animate);
-    document.querySelector('.threejs-cube').appendChild(renderer.domElement);
+    camera.position.z = 5;
 
+    // cube
     const geometry = new THREE.BoxGeometry(1, 1, 1);
     const material = new THREE.MeshBasicMaterial({ color: 0x0000ff });
     const cube = new THREE.Mesh(geometry, material);
 
-		const edges = new THREE.EdgesGeometry(geometry);
-		const lineMaterial = new THREE.LineBasicMaterial({ color: 0x000000, linewidth: 2 })
-		const border = new THREE.LineSegments(edges, lineMaterial);
-		cube.add(border);
-		scene.add(cube);
+    // borders
+    const edges = new THREE.EdgesGeometry(geometry);
+    const lineMaterial = new THREE.LineBasicMaterial({ color: 0x000000, linewidth: 2 });
+    const border = new THREE.LineSegments(edges, lineMaterial);
+    cube.add(border);
 
-    camera.position.z = 5;
+    scene.add(cube);
+
+    // render
+    const renderer = new THREE.WebGLRenderer();
+    renderer.setSize(window.innerWidth, window.innerHeight);
+    renderer.setAnimationLoop(animate);
 
     function animate() {
       cube.rotation.x += 0.01;
       cube.rotation.y += 0.01;
       renderer.render(scene, camera);
     }
+
+    const container = document.querySelector('.threejs-cube');
+    container.appendChild(renderer.domElement);
+
+    // cleanup on unmount
+    return () => {
+      renderer.dispose();
+      container.removeChild(renderer.domElement);
+    };
   }, []);
 
   return <div className="threejs-cube"></div>;
