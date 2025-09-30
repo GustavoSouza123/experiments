c61d3945d4e48af5f6db88c52182d17f90d01fad
Gustavo Souza <gustavoelia7@gmail.com>
2025-09-30 19:03:35 -0300

    horizontal gallery for architecture studio portfolio

--- src/pages/experiments/architecture-studio-portfolio/ArchitectureStudioPortfolio.jsx
+++ src/pages/experiments/architecture-studio-portfolio/ArchitectureStudioPortfolio.jsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from 'react';
+import React, { useEffect, useRef, useState } from 'react';
 import h01 from './images/h01.jpeg';
 import h02 from './images/h02.jpeg';
 import h03 from './images/h03.jpeg';
@@ -11,68 +11,100 @@ import v03 from './images/v03.jpg';
 import v04 from './images/v04.jpg';
 import v05 from './images/v05.jpg';
 import './style.scss';
+import gsap from 'gsap';
+import { useGSAP } from '@gsap/react';
+import { CustomEase, ScrollTrigger } from 'gsap/all';
+
+gsap.registerPlugin(ScrollTrigger, CustomEase, useGSAP);
 
 export default function ArchitectureStudioPortfolio() {
   const [scrollProgress, setScrollProgress] = useState(0);
+  const triggerRef = useRef(null);
+  const worksRef = useRef(null);
 
   const works = [
     {
-      name: 'jaloura main house',
-      image: h01,
-    },
-    {
-      name: 'jaloura main house',
-      image: h02,
-    },
-    {
-      name: 'jaloura main house',
-      image: h03,
-    },
-    {
-      name: 'jaloura main house',
+      name: 'vernon',
       image: h04,
     },
     {
-      name: 'jaloura main house',
-      image: h05,
+      name: 'warraba 2',
+      image: v05,
     },
     {
-      name: 'jaloura main house',
-      image: h06,
-    },
-    {
-      name: 'jaloura main house',
+      name: 'Myrtle Pool House',
       image: v01,
     },
     {
-      name: 'jaloura main house',
-      image: v02,
+      name: 'lloyd',
+      image: h01,
     },
     {
-      name: 'jaloura main house',
-      image: v03,
+      name: 'Clifton',
+      image: h02,
     },
     {
-      name: 'jaloura main house',
+      name: 'upper cairns terrace',
       image: v04,
     },
     {
-      name: 'jaloura main house',
-      image: v05,
+      name: 'mowbray',
+      image: v02,
+    },
+    {
+      name: 'tooth',
+      image: h03,
+    },
+    {
+      name: 'ozanne',
+      image: h05,
+    },
+    {
+      name: 'hawken',
+      image: v03,
+    },
+    {
+      name: 'Haig',
+      image: h06,
     },
   ];
 
-  useEffect(() => {
-    window.addEventListener('scroll', handleScroll);
+  useGSAP(() => {
+    const trigger = triggerRef.current;
+    const section = worksRef.current;
 
-    return () => {
-      window.removeEventListener('scroll', handleScroll);
-    };
-  }, []);
+    const totalWidth = section.scrollWidth;
+    const viewportWidth = window.innerWidth;
+
+    gsap.to(section, {
+      x: -(totalWidth - viewportWidth),
+			ease: 'none',
+      scrollTrigger: {
+        trigger: trigger,
+        start: 'top top',
+        end: () => `${totalWidth - viewportWidth}`,
+        pin: true,
+        pinSpacing: false,
+        scrub: true,
+        anticipatePin: 1,
+        markers: true,
+        onUpdate: (self) => {
+          setScrollProgress(Math.round(self.progress * 100));
+        },
+      },
+    });
+  });
+
+  // useEffect(() => {
+  //   window.addEventListener('scroll', handleScroll);
+
+  //   return () => {
+  //     window.removeEventListener('scroll', handleScroll);
+  //   };
+  // }, []);
 
   const handleScroll = () => {
     // const worksEl = document.querySelector('.gallery .works');
-
     // const totalHeight = worksEl.scrollHeight - worksEl.clientWidth;
     // const scrolled = window.scrollY;
     // const progress = (scrolled / totalHeight) * 100;
@@ -80,8 +112,8 @@ export default function ArchitectureStudioPortfolio() {
   };
 
   return (
-    <div className="container">
-      <header>
+    <div className="architecture-studio-portfolio" ref={triggerRef}>
+      <header className="grid">
         <div className="name">OH Architecture</div>
 
         <nav>
@@ -102,15 +134,17 @@ export default function ArchitectureStudioPortfolio() {
         </nav>
 
         <div className="contact">
-          Get in touch <span></span>
+          <div className="button">
+            Get in touch <span></span>
+          </div>
         </div>
       </header>
 
       <main>
         <section className="gallery">
-          <div className="works">
-            {works.map((work) => (
-              <div className="work">
+          <div className="works" ref={worksRef}>
+            {works.map((work, index) => (
+              <div className="work" key={index}>
                 <div className="image">
                   <img src={work.image} alt={work.image + ' image'} />
                 </div>
@@ -120,15 +154,20 @@ export default function ArchitectureStudioPortfolio() {
           </div>
         </section>
 
-        <section className="bottom">
+        <section className="bottom grid">
           <div className="title">Works</div>
           <div className="scroll">
             <div className="text">Scroll down to explore</div>
-            <div className="progress">({scrollProgress})</div>
+            <div className="progress">
+              ({scrollProgress}
+              <span>%</span>)
+            </div>
           </div>
           <div className="links">
-            <div className="link">in progress ↗</div>
-            <div className="link">archive ↗</div>
+            <div className="buttons">
+              <div className="link">in progress ↗</div>
+              <div className="link">archive ↗</div>
+            </div>
           </div>
         </section>
       </main>
