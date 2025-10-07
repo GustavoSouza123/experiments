b0d2ab3c8ac89aa558656e269bef3221db60c273
Gustavo Souza <gustavoelia7@gmail.com>
2025-10-07 18:21:45 -0300

    g1 experience intro animation

--- src/pages/experiments/g1-experience/G1Experience.jsx
+++ src/pages/experiments/g1-experience/G1Experience.jsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useRef } from 'react';
 import gsap from 'gsap';
 import { useGSAP } from '@gsap/react';
 import { ScrollTrigger, CustomEase, SplitText } from 'gsap/all';
@@ -7,26 +7,116 @@ import './style.scss';
 gsap.registerPlugin(ScrollTrigger, SplitText, CustomEase);
 
 export default function G1Experience() {
-  useGSAP(() => {});
+  const sideMenuRef = useRef(null);
+  const maskRef = useRef(null);
+  const circleRefs = useRef([]);
+  const titleRef = useRef(null);
+  const textRefs = useRef([]);
+
+  useGSAP(() => {
+    let tl = gsap.timeline({
+      defaults: {
+        // ease: CustomEase.create('custom', 'M0,0 C0.19,1 0.22,1 1,1'),
+        ease: CustomEase.create('custom', 'M0,0 C0.22,1 0.36,1 1,1'),
+        duration: 1.5,
+      },
+    });
+
+    tl.from(
+      maskRef.current,
+      {
+        yPercent: -100,
+      },
+      '0.5'
+    ).from(
+      circleRefs.current,
+      {
+        scale: 0,
+        stagger: 0.2,
+      },
+      '<0.1'
+    );
+
+    SplitText.create(titleRef.current, {
+      type: 'chars',
+      mask: 'chars',
+      charsClass: 'char++',
+
+      onSplit: (self) => {
+        tl.from(
+          self.chars,
+          {
+            yPercent: 100,
+            stagger: 0.05,
+          },
+          '>-1'
+        );
+      },
+    });
+
+    SplitText.create(textRefs.current, {
+      type: 'words',
+      mask: 'words',
+      wordsClass: 'word++',
+
+      onSplit: (self) => {
+        tl.from(
+          self.words,
+          {
+            yPercent: 100,
+            stagger: 0.2,
+          },
+          '>-1.25'
+        );
+      },
+    });
+
+    tl.from(
+      sideMenuRef.current,
+      {
+        xPercent: 100,
+      },
+      '<'
+    );
+  });
 
   return (
     <div className="experiment g1-experience">
       <div className="content">
         <div className="hero">
           <div className="top grid">
-            <div className="title">G1 Experience</div>
-            <div className="edition">
-              <div className="text">3rd</div>
-              <div className="text">edition</div>
+            <div className="title" ref={titleRef}>
+              G1 Experience
             </div>
+            <div className="edition">
+              <div
+                className="text"
+                ref={(el) => el && !textRefs.current.includes(el) && textRefs.current.push(el)}
+              >
+                3rd
+              </div>
+              <div
+                className="text"
+                ref={(el) => el && !textRefs.current.includes(el) && textRefs.current.push(el)}
+              >
+                edition
+              </div>
+            </div>
+            <div className="mask" ref={maskRef}></div>
           </div>
           <div className="bottom">
-            <div className="circle"></div>
-            <div className="circle"></div>
+            <div
+              className="circle"
+              ref={(el) => el && !circleRefs.current.includes(el) && circleRefs.current.push(el)}
+            ></div>
+            <div
+              className="circle"
+              ref={(el) => el && !circleRefs.current.includes(el) && circleRefs.current.push(el)}
+            ></div>
           </div>
         </div>
       </div>
-      <div className="side-menu">
+      <div className="side-menu" ref={sideMenuRef}>
         <div className="menu-toggle">
           <span></span>
           <span></span>
