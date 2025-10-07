244774e32099e29f563ceb06d7f805ba423391ae
Gustavo Souza <gustavoelia7@gmail.com>
2025-10-07 13:05:14 -0300

    g1 experience styles and fonts

--- src/pages/experiments/g1-experience/G1Experience.jsx
+++ src/pages/experiments/g1-experience/G1Experience.jsx
@@ -12,10 +12,10 @@ export default function G1Experience() {
   return (
     <div className="experiment g1-experience">
       <div className="content">
-        <div className="hero grid">
-          <div className="top">
+        <div className="hero">
+          <div className="top grid">
             <div className="title">G1 Experience</div>
-            <div className="edition grid">
+            <div className="edition">
               <div className="text">3rd</div>
               <div className="text">edition</div>
             </div>
@@ -32,21 +32,26 @@ export default function G1Experience() {
           <span></span>
         </div>
         <div className="content">
-          <nav>
-            <ul>
-              <li>
-                <a href="">Schedule</a>
-              </li>
-              <li>
-                <a href="">Talks</a>
-              </li>
-              <li>
-                <a href="">Exhibition</a>
-              </li>
-            </ul>
-          </nav>
+          <div className="title">
+            <span>G1</span> <span>Experience</span>
+          </div>
+          <div className="bottom">
+            <nav>
+              <ul>
+                <li>
+                  <a href="">Schedule</a>
+                </li>
+                <li>
+                  <a href="">Talks</a>
+                </li>
+                <li>
+                  <a href="">Exhibition</a>
+                </li>
+              </ul>
+            </nav>
+            <div className="cta">Get Tickets</div>
+          </div>
         </div>
-        <div className="cta">Get Tickets</div>
       </div>
     </div>
   );
